ed16e07274ccde51fabf4742347096c8ecf1f9dd
Ilya Zakharov <2146376@gmail.com>
2023-10-01 18:17:48 +0300

    resat mane of function

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from requests import session
 from time import sleep
 from fake_useragent import UserAgent
 
-def give_inform():
+def open_URL(url):
     ua = UserAgent()
     headers = {'accept': '*/*', 'user-agent': ua.firefox}
     gfdhjbgdj = 1
-    url = "https://www.propertyfinder.ae/en/search?c=1&ob=nd&page=1"
     r = requests.get(url, headers=headers)
     page = BeautifulSoup(r.text, 'lxml')
     card = page.find("main")
     print(card)
     return gfdhjbgdj
 
-give_inform()
+
+url = "https://www.propertyfinder.ae/en/search?c=1&ob=nd&page=1"
+open_URL(url)
