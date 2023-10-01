90812628114c157365638f5a65e57f7554ab352a
Ilya Zakharov <2146376@gmail.com>
2023-10-01 18:14:26 +0300

    reset return

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from fake_useragent import UserAgent
 def give_inform():
     ua = UserAgent()
     headers = {'accept': '*/*', 'user-agent': ua.firefox}
-    count = 1
+    gfdhjbgdj = 1
     url = "https://www.propertyfinder.ae/en/search?c=1&ob=nd&page=1"
     r = requests.get(url, headers=headers)
     page = BeautifulSoup(r.text, 'lxml')
     card = page.find("main")
     print(card)
+    return gfdhjbgdj
 
 give_inform()
